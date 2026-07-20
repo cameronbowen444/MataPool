b7335abeeaa7a164f5246db91b735e236041f06c
Ava Harken <avaharken@gmail.com>
2026-07-20 16:46:41 -0700

    added link in cors_allowed_origins array, added log in placeholder

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -131,5 +131,7 @@ STATIC_URL = 'static/'
 #ADD ANOTHER IF NEEDED
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:8000/"
+    "http://127.0.0.1:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
 ]
